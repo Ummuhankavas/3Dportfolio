339d916a795afc2192f13b925fd041db5b15807c
UmmuhanKavas <ummuhankavas@gmail.com>
2023-03-09 21:30:45 +0100

    p tag added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,10 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
           <div>
-            <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className='text-[#915eff]'>Ümmühan</span></h1>
+            <h1 className={`${styles.heroHeadText}`}>Hi,I'm <span className='text-[#915eff]'>Ümmühan</span></h1>
+            <p className={`${styles.heroSubText} mt-1 text-white-100`}>
+              I develop 3D visuals, user <br className='sm:block hidden'/>interfaces and web applications
+            </p>
           </div>
 
         
