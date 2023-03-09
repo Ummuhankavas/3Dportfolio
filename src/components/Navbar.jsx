a1da610c7777929e0cefa0bbe9ccbd337addd425
UmmuhanKavas <ummuhankavas@gmail.com>
2023-03-09 19:19:36 +0100

    navbar created and about  work contact added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { navLinks } from '../constans';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -22,12 +23,32 @@ const Navbar = () => {
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
-            <li>
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                  ? 'text-white'
+                  : 'text-secondary'
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
 
         </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+              <img
+                src={toggle ? close : menu}
+                alt='menu'
+                className='w-[28px] h-[28px] object-contain cursor-pointer'
+                onClick={()=> setToggle(!toggle) }
+              />
+
+              <div >
+
+              </div>
+
+        </div>
 
       </div>
     </nav>
